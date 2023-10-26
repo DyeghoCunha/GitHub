78737821029612717d7e0c4c0379c9e57882f912
Dyegho <dyeghocunha@gmail.com>
2023-10-26 19:28:23 -0300

    feat

--- simples_nacional_v3/lib/pages/PaginaPrincipal_page.dart
+++ simples_nacional_v3/lib/pages/PaginaPrincipal_page.dart
@@ -1,23 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:simples_nacional_v3/components/sections/account_actions.dart';
+
+import '../themes/images.dart';
 
 class PaginaPrincipalPage extends StatelessWidget {
   const PaginaPrincipalPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Card(
-        child: Container(
-          width: 300,
-          height: 200,
-          decoration:  BoxDecoration(
-            borderRadius: BorderRadius.circular(6),
-              image: const DecorationImage(
-            image: AssetImage("assets/images/cardPadrao.png"),
-            fit: BoxFit.cover,
-          )),
+    List<String> fundoLista = Theme.of(context).brightness == Brightness.light
+        ? Imagens.cardFuncoesLight
+        : Imagens.cardFuncoesLight;
+
+    return Container(
+
+      child: GridView.custom(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 1,
+        ),
+        childrenDelegate: SliverChildListDelegate(
+          [
+             const _cardFuncoesRow(),
+          ],
         ),
       ),
+    );
+  }
+}
+
+class _cardFuncoesRow extends StatelessWidget {
+  const _cardFuncoesRow({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    List<String> fundoLista = Theme.of(context).brightness == Brightness.light
+        ? Imagens.cardFuncoesLight
+        : Imagens.cardFuncoesLight;
+    return CustomScrollView(
+      scrollDirection: Axis.horizontal,
+      slivers: [
+        SliverList(
+            delegate: SliverChildListDelegate([
+          for (var fundo in fundoLista)
+            _cardFuncoes(
+              fundo: fundo,
+            )
+        ]))
+      ],
+    );
+  }
+}
+
+class _cardFuncoes extends StatelessWidget {
+  _cardFuncoes({super.key, required this.fundo});
+
+  String fundo;
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      children: [
+        Card(
+          child: Container(
+            height: 100,
+            width: 150,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(6),
+              image: DecorationImage(
+                image: AssetImage(fundo),
+                fit: BoxFit.fill,
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
---
+++ simples_nacional_v3/lib/themes/images.dart
@@ -0,0 +1,33 @@
+
+
+class Imagens {
+
+  Imagens();
+
+
+ static List<String> cardFuncoesLight = [
+    "assets/images/cardFaturamento.png",
+   "assets/images/cardAliquota.png",
+    "assets/images/cardImposto.png",
+    "assets/images/cardGraficos.png",
+    "assets/images/cardPfPj.png",
+    "assets/images/cardMei.png",
+    "assets/images/cardRescisao.png",
+    "assets/images/cardIcms.png",
+    "assets/images/cardNcm.png",
+    "assets/images/cardCfop.png",
+  ];
+
+  static List<String> cardFuncoesDark= [
+    "assets/images/cardFaturamentoD.png",
+    "assets/images/cardAliquotaD.png",
+    "assets/images/cardImpostoD.png",
+    "assets/images/cardGraficosD.png",
+    "assets/images/cardPfPjD.png",
+    "assets/images/cardMeiD.png",
+    "assets/images/cardRescisaoD.png",
+    "assets/images/cardIcmsD.png",
+    "assets/images/cardNcmD.png",
+    "assets/images/cardCfopD.png",
+  ];
+}
