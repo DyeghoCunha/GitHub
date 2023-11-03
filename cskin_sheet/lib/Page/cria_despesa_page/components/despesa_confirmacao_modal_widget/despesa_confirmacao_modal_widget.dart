3c741aa4858e350cac9a6946e9e703763de93060
Dyegho <dyeghocunha@gmail.com>
2023-11-03 13:14:12 -0300

    feat

--- cskin_sheet/lib/Page/cria_despesa_page/components/despesa_confirmacao_modal_widget/despesa_confirmacao_modal_widget.dart
+++ cskin_sheet/lib/Page/cria_despesa_page/components/despesa_confirmacao_modal_widget/despesa_confirmacao_modal_widget.dart
@@ -1,4 +1,5 @@
 import 'package:cskin_sheet/Model/despesa.dart';
+import 'package:cskin_sheet/Page/cria_despesa_page/components/despesa_card_widget/despesa_card_widget_fundo.dart';
 import 'package:flutter/material.dart';
 
 class DespesaConfirmacaoModalWidget extends StatelessWidget {
@@ -10,29 +11,119 @@ class DespesaConfirmacaoModalWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Widget text(String string) => Text(
+          string,
+          textAlign: TextAlign.start,
+        );
+
+    Widget textSpan(String title, String subtitle) => Text.rich(
+          TextSpan(
+            text: title,
+            style: TextStyle(
+                fontSize: 12, color: Theme.of(context).textTheme.bodySmall!.color!.withOpacity(0.5)),
+            children: [
+              TextSpan(
+                text: subtitle,
+                style: TextStyle(fontSize: 16, color: Theme.of(context).textTheme.bodySmall!.color!),
+              ),
+            ],
+          ),
+        );
+
     return Container(
-      child:const  Column(
-        children: [
-          ListTile(
-            title: Text(
-              "Registro e Abertura",
-              style: TextStyle(),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        textSpan("Sócio: ", "Lucas"),
+                        textSpan("Data: ", "31/12/2023"),
+                        textSpan("Valor: ", "R\$ 140.230,50"),
+                        textSpan("Operação: ", "Entrada"),
+                        textSpan("Categoria: ", "Registro e Abertura"),
+                      ],
+                    ),
+                  ),
+                  DespesaCardWidgetFundo(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: const ButtonStyle(
+                              surfaceTintColor: MaterialStatePropertyAll<Color>(Colors.green),
+                              side: MaterialStatePropertyAll<BorderSide>(BorderSide(color: Colors.green,
+                                  width: 1))
+                            ),
+                            child: const Icon(
+                              Icons.send,
+                              color: Colors.green,
+                            ),
+                          ),
+                          TextButton(
+                              style: ButtonStyle(
+                                side: MaterialStatePropertyAll<BorderSide>(
+                                  BorderSide(
+                                      style: BorderStyle.solid,
+                                      width: 0.8,
+                                      color: Theme.of(context).colorScheme.primary.withOpacity(0.4)),
+                                ),
+                              ),
+                              onPressed: () {},
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                                child: Icon(
+                                  Icons.edit,
+                                  color: Theme.of(context).colorScheme.primary.withOpacity(0.5),
+                                ),
+                              )),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
-            subtitle: Text("Valor Paga para a abertura da empresa e eu estou enrolando para fazer um texto "
-                "bem grande que provavelmente eu nunca vo uescrever tanto assim",textAlign: TextAlign.start,
-                ),
-            leading: Text("Socio"),
-            trailing: Column(
-              children: [
-                Card(child: Padding(
-                  padding: EdgeInsets.fromLTRB(10, 4, 10, 4),
-                  child: Text("R\$ 140.230,20",style: TextStyle(fontSize: 15),textAlign: TextAlign.center,),
-                )),
-                Text("10/21/2023",style: TextStyle(fontSize: 13),),
-              ],
+            const Divider(),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: textSpan(
+                  "Descrição: ",
+                  "Bla bla bla bla bla bla bla bla bla bla bla bla bla bla "
+                      "blaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"),
             ),
-          )
-        ],
+            const Divider(),
+            Container(
+                width: double.infinity,
+                child: ElevatedButton.icon(
+                  style: ButtonStyle(
+                      surfaceTintColor: MaterialStatePropertyAll<MaterialColor>(Colors.red),
+                      side: MaterialStatePropertyAll<BorderSide>(BorderSide(
+                        color: Colors.redAccent.withOpacity(0.5),
+                        width: 1,
+                      ))),
+                  onPressed: () {},
+                  label: Text(
+                    "Excluir",
+                    style: TextStyle(color: Colors.redAccent),
+                  ),
+                  icon: Icon(
+                    Icons.delete,
+                    color: Colors.redAccent,
+                  ),
+                ))
+          ],
+        ),
       ),
     );
   }
