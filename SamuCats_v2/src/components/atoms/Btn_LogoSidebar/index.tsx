b72c81b9237ecbbe4f9272ad485565d0278d73dc
Dyegho M C G Cunha <dyeghocunha@gmail.com>
2024-03-22 12:14:13 -0300

    feat(layout) : Responsividade

--- SamuCats_v2/src/components/atoms/Btn_LogoSidebar/index.tsx
+++ SamuCats_v2/src/components/atoms/Btn_LogoSidebar/index.tsx
@@ -1,7 +1,9 @@
+"use client"
 import { Avatar, Box, Button, HStack, Text } from '@chakra-ui/react'
 import Logo from "@/assets/image/logoSamucats.png"
 import { motion } from "framer-motion"
 import { ChevronRightIcon } from '@chakra-ui/icons';
+
 
 const MotionIcon = motion(ChevronRightIcon)
 
@@ -12,7 +14,7 @@ export default function Btn_LogoSidebar({ isOpen, toggleOpen }: any) {
         <Avatar size="md" src={Logo.src} />
         <Text fontSize="30px" fontWeight="bold">SamucaDoJava</Text>
         <Button onClick={()=>toggleOpen()}  w="20px" h="40px" variant="primary" borderRadius="full" position="absolute" right="-5">
-          <MotionIcon boxSize="10" animate={{ rotate: isOpen ? 180 : 0 }} transition={{ duration: 0.4 }}/>
+          <MotionIcon boxSize={10} animate={{ rotate: isOpen ? 180 : 0 }} transition={{ duration: 0.4 }}/>
         </Button>
       </HStack>
     </Box>
