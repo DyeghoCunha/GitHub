83a7e69be2e1504b810847e874e153283f02af13
dyeghocunha <dyeghocunha@gmail.com>
2024-05-23 09:12:18 -0300

    footer

--- hermes_cont/src/components/organisms/Footer/index.tsx
+++ hermes_cont/src/components/organisms/Footer/index.tsx
@@ -11,21 +11,23 @@ import mailImg from "/public/images/email.png"
 import SvgBezierCurve from "@/components/SvgBezierCurve/SvgBezierCurve"
 
 const Endereco = () => {
-  
- const [isLargerThan800] = useMediaQuery('(min-width: 800px)');
+
+  const [isLargerThan800] = useMediaQuery('(min-width: 800px)');
   return (
-    <VStack zIndex={{ base: 100, md: 10, xxl: 100 }} color="white" fontSize="20px" justify="start" align="start" mr="5%">
+    <VStack w={{base:"100%",md:"610px"}}  zIndex={{ base: 100, md: 10, xxl: 100 }} color="white" fontSize="20px" justify="start" align="start" mr={{base:"0", md:"5%"}}>
       <Map />
-      <GradientText {{ fontSize: isLargerThan800 ? "30px" : "10px" }}>Endereço</GradientText>
-      <HStack gap="20px" fontSize={{ base: "10px", md: "22px" }}>
+      <Box px={{base:"10px",md:"0"}} >
+      <GradientText style={{ fontSize: isLargerThan800 ? "30px" : "10px" }}>Endereço</GradientText>
+      </Box>
+      <HStack gap="20px" fontSize={{ base: "10px", md: "22px" }} px={{base:"10px",md:"0"}}>
         <Text>Rua: 2028 </Text>
         <Text>nº: 100 </Text>
         <Text>Sala 01 </Text>
         <Text>Cep: 88330-486</Text>
       </HStack>
-      <Text fontSize={{ base: "10px", md: "22px" }}>Balneário Camboriú - Santa Catarina</Text>
+      <Text fontSize={{ base: "10px", md: "22px" }} px={{base:"10px",md:"0"}}>Balneário Camboriú - Santa Catarina</Text>
 
-    </VStack>)
+    </VStack >)
 }
 
 
@@ -34,15 +36,17 @@ export default function Footer() {
   const [isLargerThan800] = useMediaQuery('(min-width: 800px)');
 
   return (
-    <Box as='footer' pb="20px" w="100vw" h="100%" bgColor="hermesBlue.400" bgImage={img.src} bgPos="top"
-      bgRepeat="no-repeat" bgSize="fill">
+    <Box as='footer' pb="20px" w="100vw" h="100%" bgColor="hermesBlue.400" 
+    bgImage={img.src} 
+    bgPos={{base:"top",md:"top"}}
+    bgRepeat="no-repeat" 
+    bgSize={{base:"contain",md:"fit"}}>
       <VStack w="100%" >
         <Box w="100%" h="100%" mb="40px" zIndex={1}>
           <SvgBezierCurve />
         </Box>
         <Stack direction={{ base: "column", md: "row" }} w="100%" h="100%" justify="space-between" align="center">
-
-          <VStack align="start" gap="20px" ml="5%">
+          <VStack align="start" justify="start"  w="100%" gap="20px" pl={{base:"10px",md:"5%"}}>
             <Link href='https://api.whatsapp.com/send?phone=554735144771&text=Ola!%20Eu%20vim%20atrav%C3%A9s%20do%20Site%20e%20estou%20com%20d%C3%BAvidas' target='_blank'>
               <HStack ><Image w={{ base: "30px", md: "60px" }} src={whatsImg.src} alt="" />
                 <Box>
